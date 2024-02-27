5e7d7701b870c2760033d8e52131ef448f21c089
Sye <119392569+Sye0001@users.noreply.github.com>
2024-02-28 00:32:33 +1000

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,15 +1,22 @@
-const border = document.getElementById('border');
-const submitButton = document.getElementById('submit-button');
-const discordInput = document.getElementById('discord-input');
 
-discordInput.addEventListener('input', () => {
-    if (discordInput.value.trim() !== '') {
+    const border = document.getElementById('border');
+    const submitButton = document.getElementById('submit-button');
+    const discordInput = document.getElementById('discord-input');
 
-        const requestdata = await servercheck.json()
-
-        if (requestdata.success == false) {
-            document.getElementById("notification").style.display = "flex"
-            return
+    discordInput.addEventListener('input', () => {
+        if (discordInput.value.trim() !== '') {
+            border.style.borderColor = 'rgba(209, 213, 219, 0.4)';
+            submitButton.style.opacity = '1'; // Set opacity to 1 when text is input
+        } else {
+            border.style.borderColor = 'rgba(209, 213, 219, 0.1)';
+            submitButton.style.opacity = '0.6'; // Set opacity back to 0.6 when text is empty
         }
+    });
 
-        window.location.href = `profile.html?id=${discordId}`;
+    submitButton.addEventListener('click', () => {
+        const discordId = discordInput.value.trim();
+        if (discordId !== '') {
+            // Redirect to profile.html with Discord ID as a query parameter
+            window.location.href = `profile.html?id=${discordId}`;
+        }
+    });
