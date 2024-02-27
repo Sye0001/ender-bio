46ae6d17b6e0b9b5c526c44d6ba9873df17fc368
Sye <119392569+Sye0001@users.noreply.github.com>
2024-02-28 00:36:28 +1000

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,22 +1,28 @@
+const border = document.getElementById('border');
+const submitButton = document.getElementById('submit-button');
+const discordInput = document.getElementById('discord-input');
 
-    const border = document.getElementById('border');
-    const submitButton = document.getElementById('submit-button');
-    const discordInput = document.getElementById('discord-input');
+discordInput.addEventListener('input', () => {
+    if (discordInput.value.trim() !== '') {
+        border.style.borderColor = 'rgba(209, 213, 219, 0.4)';
+        submitButton.style.opacity = '1'; // Set opacity to 1 when text is input
+    } else {
+        border.style.borderColor = 'rgba(209, 213, 219, 0.1)';
+        submitButton.style.opacity = '0.6'; // Set opacity back to 0.6 when text is empty
+    }
+});
+submitButton.addEventListener('click', async function() {
+    const discordId = discordInput.value.trim();
+    
+    if (discordId !== '') {
+        const servercheck = await fetch(`https://api.lanyard.rest/v1/users/${discordId}`)
+        const requestdata = await servercheck.json()
 
-    discordInput.addEventListener('input', () => {
-        if (discordInput.value.trim() !== '') {
-            border.style.borderColor = 'rgba(209, 213, 219, 0.4)';
-            submitButton.style.opacity = '1'; // Set opacity to 1 when text is input
-        } else {
-            border.style.borderColor = 'rgba(209, 213, 219, 0.1)';
-            submitButton.style.opacity = '0.6'; // Set opacity back to 0.6 when text is empty
+        if (requestdata.success == false) {
+            document.getElementById("notification").style.display = "flex"
+            return
         }
-    });
 
-    submitButton.addEventListener('click', () => {
-        const discordId = discordInput.value.trim();
-        if (discordId !== '') {
-            // Redirect to profile.html with Discord ID as a query parameter
-            window.location.href = `profile.html?id=${discordId}`;
-        }
-    });
+        window.location.href = `profile.html?id=${discordId}`;
+    }
+});
